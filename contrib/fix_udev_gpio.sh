4148b9ffe11901728493b754a3db481cc5c31e2e
Carl J. Nobile <carl.nobile@gmail.com>
2014-05-15 22:13:16 -0400

    Added a few mor files to fix_udev_gpio.sh

--- contrib/fix_udev_gpio.sh
+++ contrib/fix_udev_gpio.sh
@@ -6,6 +6,10 @@ find /sys/devices/virtual/gpio -type d -exec chmod 2775 {} \;
 find /sys/devices/virtual/gpio -name "uevent" -exec chmod 0660 {} \;
 find /sys/devices/virtual/gpio -name "autosuspend_delay_ms" -exec chmod 0660 {} \;
 find /sys/devices/virtual/gpio -name "control" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "direction" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "edge" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "value" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "active_low" -exec chmod 0660 {} \;
 
 # Fix the /sys/class/gpio path.
 chown -R debian:gpio /sys/class/gpio
