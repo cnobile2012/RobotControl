20ef404c0a0a0173820faddb449dfc6a19259bb8
Carl J. Nobile <carl.nobile@gmail.com>
2014-05-15 20:50:28 -0400

    Rewrote the dev rules completely and added an executable program also.

---
+++ contrib/fix_udev_gpio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fix the /sys/devices/virtual/gpio path.
+chown -R debian:gpio /sys/devices/virtual/gpio
+find /sys/devices/virtual/gpio -type d -exec chmod 2775 {} \;
+find /sys/devices/virtual/gpio -name "uevent" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "autosuspend_delay_ms" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "control" -exec chmod 0660 {} \;
+
+# Fix the /sys/class/gpio path.
+chown -R debian:gpio /sys/class/gpio
+chmod 0220 export unexport
