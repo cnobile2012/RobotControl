1087851ae7d4a8b24f7819e7f043a9f1fa0a9fca
Carl J. Nobile <carl.nobile@gmail.com>
2014-05-16 21:35:17 -0400

    Optimized the order of the files and drectories changed when the rule fires.

--- contrib/fix_udev_gpio.sh
+++ contrib/fix_udev_gpio.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+#
+# This file will change the user, group and permissions in both the
+# /sys/devices/virtual/gpio and /sys/class/gpio path directories.
+#
+# DO NOT change the order of the commands below, they are optimized so that
+# commonly created files and directories are changed first.
+#
 
-# Fix the /sys/devices/virtual/gpio path.
 chown -R debian:gpio /sys/devices/virtual/gpio
+chown -R debian:gpio /sys/class/gpio
 find /sys/devices/virtual/gpio -type d -exec chmod 2775 {} \;
-find /sys/devices/virtual/gpio -name "uevent" -exec chmod 0660 {} \;
-find /sys/devices/virtual/gpio -name "autosuspend_delay_ms" -exec chmod 0660 {} \;
-find /sys/devices/virtual/gpio -name "control" -exec chmod 0660 {} \;
 find /sys/devices/virtual/gpio -name "direction" -exec chmod 0660 {} \;
 find /sys/devices/virtual/gpio -name "edge" -exec chmod 0660 {} \;
 find /sys/devices/virtual/gpio -name "value" -exec chmod 0660 {} \;
-find /sys/devices/virtual/gpio -name "active_low" -exec chmod 0660 {} \;
 
-# Fix the /sys/class/gpio path.
-chown -R debian:gpio /sys/class/gpio
+find /sys/devices/virtual/gpio -name "active_low" -exec chmod 0660 {} \;
 chmod 0220 /sys/class/gpio/export
 chmod 0220 /sys/class/gpio/unexport
+find /sys/devices/virtual/gpio -name "uevent" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "autosuspend_delay_ms" -exec chmod 0660 {} \;
+find /sys/devices/virtual/gpio -name "control" -exec chmod 0660 {} \;
